aa700efb3474df4f335b5f85d75f2bd6fec6c9c8
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2024-05-11 15:49:12 +0530

    modified alternate code

--- src/index.js
+++ src/index.js
@@ -29,25 +29,23 @@ connectDB()
 
 
 
+// Alternate way to connect with DB (writing code direcltly in index.js)
 
-
-
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants"
 // import express from "express"
 // const app = express()
 
-//     ; (async () => {
-//         try {
-//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//             app.on("error", (error) => {
-//                 console.log("Error: ", error);
-//                 throw error
-//             })
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         console.log("Connected to MongoDB");
 
-//             app.listen(process.env.PORT, () => {
-//                 console.log('App is lis');
-//             })
-//         } catch (error) {
-//             console.log("Error: ", error);
-//             throw error
-//         }
-//     })()
+//         app.listen(process.env.PORT, () => {
+//             console.log('App is listening on port:', process.env.PORT);
+//         });
+//     } catch (error) {
+//         console.error("Error: ", error);
+//         process.exit(1); // Exit the process with a non-zero exit code to indicate failure
+//     }
+// })();
